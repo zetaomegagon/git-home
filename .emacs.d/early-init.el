09a200d2c5e97e794bae57cf4fe58b8dac635ef7
Elijah Beale <elijahthomasbeale+github@gmail.com>
2023-02-03 22:51:45 -0500

    Straight.el configuration
    .emacs.d/init.el
    * added straight.el bootstrap code
    .emacs.d/early-init.el
    * initial commit
    * inhibit package.el as per straight.el docs

---
+++ .emacs.d/early-init.el
@@ -0,0 +1,2 @@
+;; inhibit package.el as required by straight.el
+(setq package-enable-at-startup nil)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,3 +27,17 @@
 ;; tree-sitter
 (require 'treesit)
 
+;; straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
