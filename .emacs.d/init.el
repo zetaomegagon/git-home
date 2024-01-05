1814cf8fcf4680a02ae1b4e678d0931e7f70c420
Elijah Beale <elijahthomasbeale+github@gmail.com>
2024-01-04 21:23:33 -0500

    ~/.bin/.emacs.d: fixed authinfo stored password; moved elpaca into early-init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -69,53 +69,11 @@
 	      (progn (setq fill-column 80)
 		     (display-fill-column-indicator-mode)))))
 
-
-;; elpaca
-(defvar elpaca-installer-version 0.6)
-(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
-(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
-(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
-(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-                              :ref nil
-                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
-                              :build (:not elpaca--activate-package)))
-(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
-       (build (expand-file-name "elpaca/" elpaca-builds-directory))
-       (order (cdr elpaca-order))
-       (default-directory repo))
-  (add-to-list 'load-path (if (file-exists-p build) build repo))
-  (unless (file-exists-p repo)
-    (make-directory repo t)
-    (when (< emacs-major-version 28) (require 'subr-x))
-    (condition-case-unless-debug err
-        (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-                 ((zerop (call-process "git" nil buffer t "clone"
-                                       (plist-get order :repo) repo)))
-                 ((zerop (call-process "git" nil buffer t "checkout"
-                                       (or (plist-get order :ref) "--"))))
-                 (emacs (concat invocation-directory invocation-name))
-                 ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
-                                       "--eval" "(byte-recompile-directory \".\" 0 'force)")))
-                 ((require 'elpaca))
-                 ((elpaca-generate-autoloads "elpaca" repo)))
-            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
-          (error "%s" (with-current-buffer buffer (buffer-string))))
-      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
-  (unless (require 'elpaca-autoloads nil t)
-    (require 'elpaca)
-    (elpaca-generate-autoloads "elpaca" repo)
-    (load "./elpaca-autoloads")))
-(add-hook 'after-init-hook #'elpaca-process-queues)
-(elpaca `(,@elpaca-order))
-
-;; Install use-package support
-(elpaca elpaca-use-package
-  (elpaca-use-package-mode)               ; Enable :elpaca use-package keyword.
-  (setq elpaca-use-package-by-default t)) ; Assume :elpaca t unless otherwise specified.
-
-(elpaca-wait)
+;; set auth-sources to only use a gpg backed sources
+(setq auth-sources '("~/.authinfo.gpg"))
 
 ;; packages
+(elpaca-wait)
 
 ;;;; org-mode
 (use-package org
@@ -240,7 +198,7 @@
 (require 'desktop)
 (desktop-read)
 (setq desktop-path (list "~/.emacs.d/desktop-save/")
-      ; set this to 0 to avoid emacs.service hitting timeout 
+      ; set this to 0 to avoid emacs.service hitting timeout
       desktop-restore-eager 0
       desktop-auto-save-timeout 5
       desktop-load-locked-desktop t
