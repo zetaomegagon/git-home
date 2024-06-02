41d898964b5703215a58b916c8fb93af4eea4cf1
Elijah Beale <elijahthomasbeale+github@gmail.com>
2024-06-02 19:26:36 -0400

    ~/.emacs.d/early-init.el: rebuilt emacs. updated elpaca-core-date

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -2,7 +2,7 @@
 (setq package-enable-at-startup nil)
 
 ;; elpaca
-(setq elpaca-core-date '(20240421)) ; https://github.com/progfolio/elpaca/wiki/Warnings-and-Errors#unable-to-determine-elpaca-core-date
+(setq elpaca-core-date '(20240602)) ; https://github.com/progfolio/elpaca/wiki/Warnings-and-Errors#unable-to-determine-elpaca-core-date
 
 
 (defvar elpaca-installer-version 0.6)
