ad47d52ee8ab47fa3fa6b0dbd6f9cb95cac830a8
Elijah Beale <elijahthomasbeale+github@gmail.com>
2024-08-13 09:38:36 -0400

    ~/.emacs.d/init.el: added eshell configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -96,6 +96,36 @@
 ;; winner mode
 (winner-mode)
 
+;; eshell
+(defun eshell-here ()
+  "Opens up a new shell in the directory associated with the current buffer's file."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (name (car (last (split-string parent "/" t)))))
+    (split-window-vertically)
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))))
+
+(global-set-key (kbd "C-c C-;") 'eshell-here)
+
+(defun delete-single-window (&optional window)
+  "Remove WINDOW from the display.  Default is `selected-window'.
+If WINDOW is the only one in its frame, then `delete-frame' too."
+  (interactive)
+  (save-current-buffer
+    (setq window (or window (selected-window)))
+    (select-window window)
+    (kill-buffer)
+    (if (one-window-p t)
+        (delete-frame)
+        (delete-window (selected-window)))))
+
+(defun eshell/x (&rest args)
+  (delete-single-window))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                           Begin Packages                                   ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
